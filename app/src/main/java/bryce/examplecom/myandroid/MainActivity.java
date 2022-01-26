2feb697f459c3e923700fcdcdaca589d804d550a
Hyesung Park <jayhspark@gmail.com>
2022-01-26 13:34:28 +0900

    first commit

---
+++ app/src/main/java/bryce/examplecom/myandroid/MainActivity.java
@@ -0,0 +1,20 @@
+package bryce.examplecom.myandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+  public void clickFunction(View view) {
+    Log.i("Info", "Button presssed!");
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}

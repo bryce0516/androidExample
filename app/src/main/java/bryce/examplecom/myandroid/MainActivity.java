624c8c076a010d93c5237b0d4dcc9e2ce468a1bd
Hyesung Park <jayhspark@gmail.com>
2022-01-26 14:09:49 +0900

    update

--- app/src/main/java/bryce/examplecom/myandroid/MainActivity.java
+++ app/src/main/java/bryce/examplecom/myandroid/MainActivity.java
@@ -5,11 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
   public void clickFunction(View view) {
+
+    EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+
     Log.i("Info", "Button presssed!");
+
+    Log.i("Values", nameEditText.getText().toString());
+
+  }
+
+  public void loginFunction(View view) {
+    Log.i("Info","LoginButton pressed");
+    EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+    EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+    Log.i("Username", usernameEditText.getText().toString());
+    Log.i("Password", passwordEditText.getText().toString());
+
+    Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show();
   }
 
   @Override
